c1325af31c1d4c8c52900858fa329fa080081157
Mike Plavsky <Mike.Plavsky@quest.com>
2012-07-18 18:27:33 +0000

    first epics got

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"], [clj-http "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"], 
+                 [clj-http "0.5.0"],
+                 [org.clojure/data.zip "0.1.1"]]
   :main pt-epics.core)
--- src/pt_epics/core.clj
+++ src/pt_epics/core.clj
@@ -1,5 +1,8 @@
 (ns pt-epics.core)
-(require '[clj-http.client :as client])
+(use '[clojure.data.zip.xml :only (attr text xml->)])
+(require '[clj-http.client :as client]
+         '[clojure.xml :as xml]
+         '[clojure.zip :as zip])
 
 (def pt-url 
   "https://www.pivotaltracker.com/services/v3/projects/%s/stories?type:feature,release")
@@ -8,6 +11,49 @@
   [id]
   (client/get (format pt-url id)
   {:headers {"X-TrackerToken" "d85f22694013ecaa075b0e938f104854"}}))
+
+(defn get-stream
+  [s]
+  (java.io.ByteArrayInputStream. (.getBytes s)))
+
+(defn values 
+  [loc,ks]
+  (assoc (map #(list % (first (xml-> loc % text))) ks))) 
+
+(defn get-labels 
+  [z]
+  (xml-> z :story [:labels] 
+         #(values % [:labels :estimate :name])))
+
+(def example 
+"
+  <stories>
+    <story>
+      <id type='Integer'>11</id>
+      <state>Finished</state>
+    </story>
+    <story>
+      <id type='Integer'>12</id>
+      <state>Started</state>
+    </story>
+  </stories>
+")
+
+(def z1 (->
+          example
+          get-stream
+          xml/parse
+          zip/xml-zip))
+
+;(def example-proj 246825)
+
+(def z (-> 
+         246825 
+         get-project-stories 
+         :body
+         get-stream 
+         xml/parse 
+         zip/xml-zip))
 
 (defn -main
   [& args]
