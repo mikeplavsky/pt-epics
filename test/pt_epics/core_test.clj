d2c4f9760c7b3fb2d3d1966f26a50d6f1d26db03
Mike Plavsky <Mike.Plavsky@quest.com>
2012-08-06 22:45:39 +0000

    test for burndown fails

--- test/pt_epics/core_test.clj
+++ test/pt_epics/core_test.clj
@@ -14,17 +14,25 @@
       <story>
         <labels> Dashboards,RESTs ,News </labels>
         <estimate>11</estimate>             
-        <accepted_at>0</accepted_at>             
+        <accepted_at>2012/05/14 11</accepted_at>             
       </story>
       <story>
         <labels>Dashboards,RESTs</labels>
         <estimate>12</estimate>             
-        <accepted_at>0</accepted_at>             
+        <accepted_at>2012/06/12 11</accepted_at>             
       </story>
       <story>
         <labels>RESTs</labels>
         <estimate>12</estimate>             
-        <accepted_at>0</accepted_at>             
+        <accepted_at>2012/07/14 11</accepted_at>             
+      </story>
+      <story>
+        <labels>Policies</labels>
+        <estimate>12</estimate>             
+      </story>
+      <story>
+        <labels>Policies</labels>
+        <estimate>10</estimate>             
       </story>
     </stories>
   </iteration>              
@@ -34,7 +42,7 @@
 (deftest test-labels
   (testing "should get right labels"
     (let  [p (zip-project project1)]
-      (is (= (get-labels p) #{"dashboards" "rests" "news"})))))
+      (is (= (get-labels p) #{"policies" "dashboards" "rests" "news"})))))
 
 (deftest test-label-weight
   (testing "should get right weight"
@@ -43,4 +51,11 @@
       (is (= (label-weight "news" s) 11))
       (is (= (label-weight "rests" s) 35))
       (is (= (label-weight "dashboards" s) 23)))))      
+
+(deftest test-burndown
+  (testing "should get right decrements"
+    (let [p [(zip-project project1)]
+          s (stories p)]
+      (is (= (burndown s) 12)))))
+
 
